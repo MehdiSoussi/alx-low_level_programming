3ce44cf21937818c08bdb83cb726e4b3245dfb03
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-05 14:13:09 -0700

    [212C5A1] 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,12 +3,12 @@
 /**
  * main - Entry point
  *
- * Description: prints using printf "with proper grammar, but the outcome is a piece of art,"
+ * Description: prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-		printf("with proper grammar, but the outcome is a piece of art,");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 }
