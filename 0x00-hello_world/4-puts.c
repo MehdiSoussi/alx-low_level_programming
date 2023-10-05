c8a08135802fe61d9db391e960d79f0e22d7a202
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-05 13:58:28 -0700

    [212C4A8] 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,13 @@
-#include<stdio.h>
+#include <stdio.h>
+/**
+ * main function : Start program
+ *
+ * Return: 0 means success
+ */
 int main(void)
 {
-    char Mystr[] = "\"Programming is like building a multilingual puzzle";     
-    puts(Mystr);       
-return (0);
+	char Mystr[] = "\"Programming is like building a multilingual puzzle";
+	puts(Mystr);
+	return (0);
+
 }
