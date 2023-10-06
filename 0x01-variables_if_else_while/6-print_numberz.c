d2358a693606151456da06b9620c44aee0e608ad
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-06 12:11:13 -0700

    [P213T6A0] 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: [P213T6]"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 48;
+	int r = 10;
+
+	do {
+		putchar (c);
+		c++;
+	} while (c <= 57);
+	putchar(r);
+
+	return (0);
+}
+
