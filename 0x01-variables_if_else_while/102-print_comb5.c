9bbf878df4601422756f6d226de8e6c28fa2fb31
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-06 13:42:17 -0700

    [P213T13A0] 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: [P213T11]"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 48;
+	int i = 48;
+	int k = 48;
+	int l = 48;
+	int v = 44;
+	int s = 32;
+	int r = 10;
+
+	for (c = 48; c <= 57; c++)
+	{
+		for (i = 48; i <= 57; i++)
+			{
+			for (k = 48; k <= 57; k++)
+			{
+				for (l = 48; l <= 57; l++)
+				{
+					if (c > i)
+					{
+						putchar (c);
+						putchar (i);
+						putchar (s);
+						putchar (k);
+						putchar (l);
+					}
+				}
+			}
+			}
+	}
+
+	putchar(r);
+
+	return (0);
+}
+
