ec10e8641718e828f26d5f1d7eb0aefe02459af7
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-06 11:47:59 -0700

    [213C3A0] 3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: [213T3]"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'a';
+	char r = '\n';
+
+
+	do {
+		putchar (c);
+		c++;
+	} while (c <= 122);
+	c = "A"
+	do {
+		putchar (c);
+		c++;
+	} while (c <= 90);
+	putchar(r);
+
+	return (0);
+}
+
