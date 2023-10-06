591e43746ff2553160d6d4ec96649a3cc3d8e294
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-06 12:26:16 -0700

    [P213T8A0] 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: [P213T8]"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c = 48;
+	int c = 97;
+	int r = 10;
+
+	do {
+		putchar (c);
+		c++;
+	} while (c <= 57);
+	do {
+		putchar (d);
+		d++;
+	} while (d <=102);
+	
+	putchar(r);
+
+	return (0);
+}
+
