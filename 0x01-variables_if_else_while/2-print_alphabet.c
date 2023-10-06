16e7aa50d0e912637e5b020c72da9e5220a5270a
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-06 11:39:14 -0700

    [213C2A0] 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: [213C2A0]"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 97;
+
+	do {
+		putchar (c);
+		c++;
+	} while (c <= 122);
+	putchar("\n");
+
+	return (0);
+}
+
