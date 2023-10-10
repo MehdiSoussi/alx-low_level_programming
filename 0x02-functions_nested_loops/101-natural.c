86bc18a2a49fb9aa76e02ee08d3b6dab4dcdfad6
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-10 15:16:23 -0700

    [P214T14A3]

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -6,7 +6,7 @@
  *     *
  *      * Return: Always 0 (Success)
  *       */
-void main(void)
+int main(void)
 {
 	int i = 1;
 	int sum = 0;
@@ -19,4 +19,5 @@ void main(void)
 		}
 	}
 	printf("%d\n", sum);
+	return (0);
 }
