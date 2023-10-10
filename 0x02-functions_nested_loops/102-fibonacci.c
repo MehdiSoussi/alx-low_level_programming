dabf0811744915ad212e40f47353709e58c795f4
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-10 15:41:06 -0700

    [P214T14A3]

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -1,33 +1,22 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Description: [P214T]
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ ** main - Entry point
+ **
+ ** Description: [P214T]
+ **
+ ** Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	unsigned int n = 0, n1 = 0, n2 = 1;
+	int i;
 
-int main() {
-	int n=0, n1=0, n2=1;
-
-	while (1 == 1)
+	for (i = 0 ; i < 50 ; i++)
 	{
 		n = n1 + n2;
 		n1 = n2;
 		n2 = n;
-		if(n <= 50)
-		{
-			if(n1+n2 > 50){
-				printf("%d", n);
-			}
-			else{
-				printf("%d, ", n);
-			}
-		}
-		else
-		{
-			break;
-		}
+		i == 50 ? printf("%d", n) : printf("%d, ", n);
 	}
 	return (0);
 }
