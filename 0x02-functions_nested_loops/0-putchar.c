023d441504c1ceeaefdb5d86eb4e33151730ad19
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-10 11:34:57 -0700

    [P214T0A0]

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ ** main - Entry point
+ **
+ ** Description: [P214T0]
+ **
+ ** Return: Always 0 (Success)
+ **/
+int main(void)
+{
+	char c[8] = "_putchar";
+	int i;
+
+	for(i=0 ; i<8 ; i++)
+	{
+		_putchar(c[i]);
+	}
+	return (0);
+}
