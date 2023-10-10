dc1ff5be59f74bd39fe591efeb2f842a9c2d0fb0
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-10 12:58:57 -0700

    [P214T6A0]

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ ** _abs - Entry point
+ **
+ ** @n : Parametr 1
+ ** Description: [P214T4]
+ **
+ ** Return: Always 0 (Success)
+ **/
+#include "main.h"
+
+int _abs(int n)
+{
+	return (n > 0 ? n : n * (-1));
+}
