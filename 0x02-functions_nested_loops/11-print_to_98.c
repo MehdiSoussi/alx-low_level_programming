beffab3c4ec374fd2b28701e389376e8359a0708
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-10 13:42:24 -0700

    [P214T11A0]

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ ** print_to_98 - Entry point
+ **
+ ** @n : Parametre 1
+ ** Description: [P214T11]
+ **
+ ** Return: Always 0 (Success)
+ **/
+void print_to_98(int n)
+{
+	int i;
+
+	if(n <= 98)
+	{
+		for(i = n ; i <= 98 ; i++)
+		{
+			if(i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	else
+	{
+		for(i = n ; i >= 98 ; i--)
+		{
+			if(i == 98)
+			{
+				printf("%d", i);
+			}
+			else
+			{
+				printf("%d, ", i);
+			}
+		}
+	}
+	printf("\n");
+}
