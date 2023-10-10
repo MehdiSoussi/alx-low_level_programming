82ca02335ce307f98e6e54d7c87129083445e52b
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-10 15:19:05 -0700

    [P214T14A3]

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Description: [P214T]
- *     *
- *      * Return: Always 0 (Success)
- *       */
+ ** main - Entry point
+ **
+ ** Description: [P214T]
+ **
+ ** Return: Always 0 (Success)
+ **/
 int main(void)
 {
 	int i = 1;
 	int sum = 0;
 
-	for(i = 1 ; i < 1024; i++)
+	for (i = 1 ; i < 1024; i++)
 	{
-		if(i % 3 == 0 || i % 5 == 0)
+		if (i % 3 == 0 || i % 5 == 0)
 		{
 			sum += i;
 		}
