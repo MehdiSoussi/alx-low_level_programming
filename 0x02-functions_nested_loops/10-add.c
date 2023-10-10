26d60866f4797a5037ef2f49702d3cde19e8cf61
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-10 13:25:54 -0700

    [P214T10A0]

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ ** add - Entry point
+ ** @a : Parametre 1
+ ** @b : Parametre 2
+ ** Description: [P214T9]
+ **
+ ** Return: Always 0 (Success)
+ **/
+
+#include <stdio.h>
+
+int add(int a, int b)
+{
+	return (a + b);
+}
