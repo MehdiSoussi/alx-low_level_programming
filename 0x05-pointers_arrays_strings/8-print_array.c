722fdb7449ae78f83e4bca038a203bac8016046b
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-16 05:30:09 -0700

    [P216T8A0]

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_array - entrypoint
+ * @a: Parametre 1
+ * @n: Paraemtre 2
+ * Description: [T8]
+ *
+ * Return: Return value
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0 ; i < n; i++)
+	{
+		printf("%d, ", *(a + i) );
+	}
+		printf("\n");
+}
