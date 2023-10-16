dc306313c53646f33f12f9730ed9d97499a74769
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-16 05:23:28 -0700

    [P216T7A0]

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+ * puts_half - entrypoint
+ * @str:  Parametre 1
+ *
+ * Description: [T7]
+ *
+ * Return: Return value
+ */
+void puts_half(char *str)
+{
+	int i;
+	int size = 0;
+	char *t = str;
+
+	while (*t != '\0')
+	{
+		size++;
+		t++;
+	}
+	if (size % 2 == 0)
+	{
+		for (i = size / 2 ; i < size; i++)
+		{
+			_putchar(*(str + i));
+		}
+	}
+	else
+	{
+		for (i =(size - 1) / 2 ; i < size; i++)
+		{
+			_putchar(*(str + i));
+		}
+	}
+}
