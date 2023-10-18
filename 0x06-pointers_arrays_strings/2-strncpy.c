230b0ae3da874bcf49889488f139203559b0cf9a
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-18 12:01:30 -0700

    [P217T2A2]

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,18 +5,52 @@
  *    * @src: Parametre 2
  *     * @n: Parametre 3
  *      *
- *       * Description: [T2]
+ *       * Description: [P216T2]
  *        *
  *         * Return: Return value
  **/
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i = 0;
+	int size;
 
-	for (i = 0; i < n; i++)
+	size = _strlen(src);
+	if (n <= size)
 	{
-		*(dest + i) = *(src + i);
+		for (i = 0; i < n; i++)
+		{
+			*(dest + i) = *(src + i);
+		}
 	}
-	*(dest + n) = '\0';
+	else
+	{
+		for (i = 0; i < size; i++)
+		{
+			*(dest + i) = *(src + i);
+		}
+		for (i = size; i < n; i++)
+		{
+			*(dest + i) = '\0';
+		}
+	}
 	return (dest);
+}
+/**
+ *  * _strlen - entrypoint
+ *   * @s: Parametre 1
+ *    *
+ *     * Description: [P216T2]
+ *      *
+ *       * Return: Return value
+ **/
+int _strlen(char *s)
+{
+	int size = 0;
+
+	while (*s != '\0')
+	{
+		size++;
+		s++;
+	}
+	return (size);
 }
