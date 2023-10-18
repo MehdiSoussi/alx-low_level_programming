ac8b52ff8b7c2b0e14c63cb904f77cd47746deca
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-18 11:43:45 -0700

    [P217T2A0]

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ *  * _strncpy - entrypoint
+ *   * @dest: Parametre 1
+ *    * @src: Parametre 2
+ *     * @n: Parametre 3
+ *      *
+ *       * Description: [T2]
+ *        *
+ *         * Return: Return value
+ **/
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	for (i = 0; i < n; i++)
+	{
+		*(dest + i) = *(src + i);
+	}
+	return (dest);
+}
