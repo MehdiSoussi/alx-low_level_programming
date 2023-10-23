ac7a20645f9ee4d7e5627eff693a5604b2e7a38f
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-23 11:57:01 -0700

    [P218T9A0]

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *  * set_string - entrypoint
+ *   * @s: Parametre 1
+ *    * @to: Parametre 2
+ *     *
+ *      * Description: [T9]
+ *       *
+ *        * Return: Return value
+ **/
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
