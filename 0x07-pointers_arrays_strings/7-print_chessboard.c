d83e3e28a853f88b2bb85a8e5afb2c5c342d6a1b
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-23 11:33:42 -0700

    [P218T7A0]

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *  * print_chessboard - entrypoint
+ *   * @s: Parametre 1
+ *    * @c: Parametre 2
+ *     *
+ *      * Description: [T7]
+ *       *
+ *        * Return: Return value
+ **/
+void print_chessboard(char (*a)[8])
+{
+	int i,j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar(10);
+	}
+}
