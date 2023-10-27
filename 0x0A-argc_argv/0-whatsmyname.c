71ac51f273a15f1495cfeca7b1c6a98140c78aab
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-27 04:23:57 -0700

    [P221T0A0]

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include "stdlib.h"
+#include "stdio.h"
+/**
+ * main - entrypoint
+ * @argc: Parametre 1
+ * @argv: Parametre 2
+ *
+ * Description: [T0]
+ *
+ * Return: Return value
+ */
+int main(__attribute__ ((unused)) int argc, char **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
