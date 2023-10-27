2354c98b287a9561f74b8867bf4b58178f8859e4
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-27 04:24:55 -0700

    [P221T1A0]

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "stdlib.h"
+#include "stdio.h"
+/**
+ * main - entrypoint
+ * @argc: Parametre 1
+ * @argv: Parametre 2
+ *
+ * Description: [T1]
+ *
+ * Return: Return value
+ */
+int main(int argc, __attribute__ ((unused)) char **argv)
+{
+	printf("%d\n", argc);
+	return (0);
+}
