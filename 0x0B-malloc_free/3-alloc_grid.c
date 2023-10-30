c5e3d5d9f641dce0ee82bf218aceb70168137b33
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-10-30 19:18:51 +0100

    [P222A10](PL)

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,7 +17,7 @@ int i, j;
 
 if (width <= 0 || height <= 0)
 return (0);
-p = malloc(height *sizeof (int *));
+p = malloc(height * sizeof (int *));
 if (p == 0)
 return (0);
 for (i = 0; i < height; i++)
@@ -26,6 +26,10 @@ for (i = 0; i < height; i++)
 if (*(p + i) == 0)
 {
 free(p);
+for(j = 0; j <= i ; j++)
+{
+free(*(p + j));
+}
 return (0);
 }
 for (j = 0; j < width; j++)
