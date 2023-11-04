4b00131ebee73431798d00253eac7d2c46f57e78
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-04 06:00:55 -0700

    [P224A7](PL)

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,13 +1,29 @@
-#ifndef HEADER
-#define HEADER
+#ifndef DOG_H
+#define DOG_H
 
+#include <stdio.h>
+#include <stdlib.h>
 
-struct dog {
-    char *name;
-    float age;
-    char *owner;
+/**
+ * *struct dog - a new type of data
+ * *representing a dog
+ * *@name: name of dog
+ * *@age: age of dog
+ * *@owner: owner's name
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 };
+
+
+/**
+ * *dog_t - Typedef for struct dog
+ **/
 typedef struct dog dog_t;
+
 #include "main.h"
 
 #endif
