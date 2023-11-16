da584518b561dd4a7c26e1daad2c300ee3149fd0
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-16 22:09:08 +0100

    0x12C 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ *  * print_list - entrypoint
+ *   * @h: Parametre 1
+ *      *
+ *       * Description: [X]
+ *        *
+ *         * Return: Return value
+ **/
+size_t print_list(const list_t *h)
+{
+list_t *counting = h;
+int count = 0;
+
+while (counting->next != 0)
+{
+count++;
+if (counting->str == 0)
+{
+printf("[0] (nil)");
+}
+else
+{
+printf("[%d] %s", counting->len, counting->str);
+}
+counting = counting->next;
+}
+return (count);
+}
