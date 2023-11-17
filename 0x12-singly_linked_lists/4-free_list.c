689454c530488a407c8cd2efd204bd9093247b12
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-17 12:00:33 +0100

    [P229T4A0]

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "lists.h"
+#include <stdlib.h>
+#include <string.h>
+/**
+ *  * free_list - entrypoint
+ *   * @head: Parametre 1
+ *      *
+ *       * Description: [X]
+ *        *
+ *         * Return: Return value
+ **/
+void free_list(list_t *head)
+{
+list_t *savetofree;
+
+while(head != 0)
+{
+savetofree = head;
+head = head-> next;
+free(savetofree);
+}
+}
