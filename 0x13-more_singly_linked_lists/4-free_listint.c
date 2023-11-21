b5ad06e50084631b946d7a5b5dd6a847689c44ef
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-21 21:45:29 +0100

    P230T4A0

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_listint - Entry point
+ *
+ * Description: Determine wether a random int is positive, negative or zero"
+ * @head: Parameter 1
+ * Return: Always 0 (Success)
+ */
+void free_listint(listint_t *head)
+{
+listint_t *holder;
+
+while(head != 0)
+{
+holder = head;
+head = head->next;
+free(holder);
+}
+}
