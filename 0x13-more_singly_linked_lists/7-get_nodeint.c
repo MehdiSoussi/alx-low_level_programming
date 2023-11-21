ed407f3ed6b6f777875262193900cc67d3b49504
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-21 22:11:47 +0100

    P230T7A0

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_nodeint_at_index - Entry point
+ *
+ * Description: Determine wether a random int is positive, negative or zero"
+ * @head: Parameter 1
+ * @index: Parameter 1
+ * Return: Always 0 (Success)
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+while(index != 0)
+{
+if(head == 0)
+return (0);
+head = head->next;
+index--;
+}
+return (head);
+}
