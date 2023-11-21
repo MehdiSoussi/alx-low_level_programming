e0f5e6f85c120f5be8234bbb1c85a5f4c1933961
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-21 22:37:48 +0100

    P230T9A5

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,8 +11,9 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new, *saveworkernext, *worker = *head;
+listint_t *new, *saveworkernext, *worker;
 
+worker = *head;
 if (idx == 0)
 {
 new = add_nodeint(head, n);
