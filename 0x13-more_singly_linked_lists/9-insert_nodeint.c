c9eb2559b4b16496357627b1a7d1c2d3642fdaa3
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-21 22:40:24 +0100

    P230T9A6

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -49,7 +49,7 @@ return (new);
  *
  * Description: Determine wether a random int is positive, negative or zero"
  * @head: Parameter 1
- * @n: Parameter 2
+ * @n: Parameter 20
  * Return: Always 0 (Success)
  */
 listint_t *add_nodeint(listint_t **head, const int n)
@@ -62,6 +62,5 @@ return (0);
 p->n = n;
 p->next = *head;
 *head = p;
-
 return (*head);
 }
