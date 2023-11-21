e604d1aba857498a679b63840658bcceb3362b92
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-21 21:09:55 +0100

    P230T0A0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Determine wether a random int is positive, negative or zero"
+ * @h: Parameter 1
+ * Return: Always 0 (Success)
+ */
+size_t print_listint(const listint_t *h)
+{
+size_t size = 0;
+while (h != 0)
+{
+size++;
+h = h-> next;
+}
+return (size);
+}
