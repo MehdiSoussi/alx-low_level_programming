9749e76036c8d38e9e0c37f1e447a7d92fdece3b
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-27 17:37:54 +0100

    PL_4

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
  * read_textfile - Entery point
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "main.h"
 /**
  * append_text_to_file - Entery point
