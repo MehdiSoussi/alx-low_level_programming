1f8e867bc1757562d8a4e3ceb0b0befbf25ac9be
MehdiSoussi <soussi.el.mehdi.98@gmail.com>
2023-11-27 17:44:46 +0100

    PL_6

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,19 +9,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-int filedescription, write, length = 0;
+int fd, w, len = 0;
 
 if (filename == NULL)
 return (-1);
+
 if (text_content != NULL)
 {
-for (length = 0; text_content[length];)
-length++;
+for (len = 0; text_content[len];)
+len++;
 }
-filedescription = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-write = write(filedescription, text_content, length);
-if (filedescription == -1 || write == -1)
+
+fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(fd, text_content, len);
+if (fd == -1 || w == -1)
 return (-1);
-close(filedescription);
+close(fd);
 return (1);
 }
